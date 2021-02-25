8474225bb5a667ea7d751335c640d23200d31e2e
support <support@Mac-mini-de-support.local>
2021-02-25 17:37:24 +0100

    Navigation and base styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Game from "./pages/Game"
+import GameOver from "./pages/GameOver"
+import HighScores from "./pages/HighScores"
+import Home from "./pages/Home"
+import Navbar from './Component/Navbar';
+import {Container} from './styled/Container'
+import {Main} from './styled/Main'
+import Globlal from './styled/Globlal'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Globlal/>
+      <Main>
+      <Container>
+        <Navbar></Navbar>
+        <Switch>
+          <Route path="/game" component={Game}/>
+          <Route path="/highScores" component={HighScores}/>
+          <Route path="/gameOver" component={GameOver}/>
+          <Route path="/" component={Home}/>
+        </Switch>
+      </Container>
+      </Main>
+    </Router>
   );
 }
 
---
+++ src/Component/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div>
+                <Link to="/">
+                    Learn.Build.<span>Type.</span>
+                </Link>
+            </div>
+                <ul>
+                    <li> <Link to="/">Home</Link> </li>
+                    <li> <Link to="/HighScores">HighScores</Link> </li>
+                </ul>
+
+            
+        </nav>
+    )
+}
