206a54d7c2bd7fd5df9d7c204b9577da528ad006
support <support@Mac-mini-de-support.local>
2021-03-12 15:22:30 +0100

    timer and gameOver

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,23 +2,40 @@ import React, {useState, useEffect} from 'react'
 import { StyledCharacter, StyledGame, StyledScore, StyledTimer } from '../styled/Game'
 import { Strong } from "../styled/Random";
 
-export default function Game() {
+export default function Game({history}) {
     const [score, setScore] = useState(0);
+    const MaxSeconds = 5;
+    const [ms, setMs] = useState(0)
+    const [seconds, setSeconds] = useState(MaxSeconds);
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setScore ((prevScore) => prevScore + 1)
-        },1000);
-        return () =>{
-            clearInterval(interval);
-        };
-    }, [score]);
+        const currentTime = new Date();
+        const interval = setInterval(() => updateTime(currentTime), 1);
+        return () => clearInterval(interval);
+    }, []);
+
+    const updateTime = (startTime) =>{
+        const endTime = new Date();
+        const passedMs = endTime.getTime() - startTime.getTime();
+        const updatedSeconds = MaxSeconds -  Math.floor(passedMs/1000) - 1;
+        const updatedMs = 1000 - passedMs.toLocaleString().slice(-3)
+        setSeconds (updatedSeconds.toLocaleString().padStart(2, '0'));
+        setMs (updatedMs.toLocaleString().padStart(3, '0'))
+    
+    }
+
+    useEffect(()=> {
+        if(seconds<= -1) {
+            history.push('/gameOver')
+        }
+
+    }, [seconds, ms])
 
     return (
         <StyledGame>
             <StyledScore>Score : <Strong>{score}</Strong></StyledScore>
             <StyledCharacter>A</StyledCharacter>
-            <StyledTimer>Time : <Strong>00:000</Strong></StyledTimer>
+            <StyledTimer>Time : <Strong>{seconds}:{ms}</Strong></StyledTimer>
         </StyledGame>
     )
 }
