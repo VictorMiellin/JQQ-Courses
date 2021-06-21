408553b6553a2a06a6b83ae06c207fb5eb7231ae
support <you@example.com>
2021-06-21 18:36:14 +0200

    ajout du filtre sur la query highScore, display et styled des highScore sur la page HighScoren

--- src/pages/HighScores.js
+++ src/pages/HighScores.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
+import { ScoreList, ScoreLI } from '../styled/HighScore';
 
 export default function HighScores() {
+    // display high those scores
+    const [highScores, setHighScores] = useState([]);
+    
+    //use fetch API to call getHighScores function
+    useEffect(() => {
+        
+        const loadHighScores = async() => {
+            try {
+                const res = await fetch('/.netlify/functions/getHighScore');
+                const scores = await res.json();
+                setHighScores(scores);
+            }catch(err){
+                console.error(err);
+            }
+        }
+        loadHighScores();
+    }, [])
+
     return (
         <div>
-            <h1>HighScores</h1>
+            <h1>High Scores</h1>
+            <ScoreList>
+                {highScores.map((score) => (
+                    <ScoreLI key={score.id}>
+                        {score.fields.Name} – {score.fields.Score}
+                    </ScoreLI>
+                ))}
+            </ScoreList>
         </div>
     )
 }
---
+++ src/styled/HighScore.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ScoreList = styled.ol`
+    //padding-left: 0;
+    //list-style: none;
+`;
+
+export const ScoreLI = styled.li`
+    font-size: 1.3rem;
+    margin-bottom: 10px
+`
